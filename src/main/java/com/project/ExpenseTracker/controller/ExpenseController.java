ef077dcb93851b525f05078f8c4ef5316e07320d
yakshitgupta310 <65021783+yakshitgupta310@users.noreply.github.com>
2025-01-02 22:57:51 +0530

    Implemented ResponseEntity

--- src/main/java/com/project/ExpenseTracker/controller/ExpenseController.java
+++ src/main/java/com/project/ExpenseTracker/controller/ExpenseController.java
@@ -5,6 +5,8 @@ import com.project.ExpenseTracker.entity.Expense;
 import com.project.ExpenseTracker.service.ExpenseService;
 import jakarta.validation.Valid;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,57 +27,93 @@ public class ExpenseController {
 
     //GET Method to retrieve all Expenses
     @GetMapping(value="/Expenses/all")
-    public List<Expense> getExpenses(){
-
-        return expenseService.getExpenses();
+    public ResponseEntity<?> getExpenses(){
+        List<Expense> all =  expenseService.getExpenses();
+        if(all!=null && !all.isEmpty() ){
+            return new ResponseEntity<>(all, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     //GET Method to retrieve Expense based on Id(Primary Key)
     @GetMapping(value="/Expenses/{id}")
-    public Optional<Expense> getExpenseById(@PathVariable("id") long id){
-
-        return expenseService.getExpenseById(id);
+    public ResponseEntity<Expense> getExpenseById(@PathVariable("id") long id){
+        Optional <Expense> expense= expenseService.getExpenseById(id) ;
+        if(expense.isPresent()){
+            return new ResponseEntity<>(expense.get(), HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     //GET Method to retrieve all Expenses based on Keyword in the Note column
     @GetMapping(value="/Expenses/ByNote/{keyword}")
-    public List<Expense> getExpensebyNoteKeyword(@PathVariable("keyword") String keyword){
+    public ResponseEntity<?> getExpensebyNoteKeyword(@PathVariable("keyword") String keyword){
         LOGGER.info("Parameter passed as part of Json Body :" + keyword);
-        return expenseService.getExpensebyNoteKeyword(keyword);
+        List<Expense> all = expenseService.getExpensebyNoteKeyword(keyword);
+        if(all!=null && !all.isEmpty()){
+            return new ResponseEntity<>(all, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     //GET Method to retrieve all Expenses for a specific date
     @GetMapping(value="/Expenses/Date")
-    public List<Expense> getExpenseByDate(@RequestParam("date") @DateTimeFormat(pattern = "dd/MM/yyyy")LocalDate date){
+    public ResponseEntity<?> getExpenseByDate(@RequestParam("date") @DateTimeFormat(pattern = "dd/MM/yyyy")LocalDate date){
         //LOGGER.info("SQL Date in controller :" + date);
-        return expenseService.getExpenseByDate(date);
+        List<Expense> all = expenseService.getExpenseByDate(date);
+        if(all!=null && !all.isEmpty()){
+            return new ResponseEntity<>(all, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 
     }
 
     //GET Method to retrieve all Expenses for the whole month with optional param of year
     @GetMapping(value="/Expenses/Month")
-    public List<Expense> getExpenseByMonth(@RequestParam("month") int month, @RequestParam(value = "year", required = false) Integer year){
+    public ResponseEntity<?> getExpenseByMonth(@RequestParam("month") int month, @RequestParam(value = "year", required = false) Integer year){
         //java.sql.Date newdate = new java.sql.Date(date.getTime());
         LOGGER.info("LocalDate month in controller :" + month);
         LOGGER.info("LocalDate year in controller :" + year);
-        return expenseService.getExpenseByMonth(month, year);
+        List<Expense> all = expenseService.getExpenseByMonth(month, year);
+        if(all!=null && !all.isEmpty()){
+            return new ResponseEntity<>(all, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     //POST Method to create an Expense
     @PostMapping(value="/Expenses")
-    public Expense createExpense(@RequestBody @Valid Expense expense){
-        return expenseService.createExpense(expense);
+    public ResponseEntity<Expense> createExpense(@RequestBody @Valid Expense expense){
+        try{
+            expenseService.createExpense(expense);
+            return new ResponseEntity<>(expense, HttpStatus.CREATED);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     //Delete Method to delete an Expense
     @DeleteMapping(value="/Expenses/{id}")
-    public String deleteExpense(@PathVariable("id") long id){
-        return expenseService.deleteExpense(id);
+    public ResponseEntity<?> deleteExpense(@PathVariable("id") long id) {
+        try {
+            expenseService.deleteExpense(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     //PUT Method to edit an Expense
     @PutMapping(value="/Expenses/{id}")
-    public Expense editExpense(@RequestBody Expense expense, @PathVariable("id") long id){
-        return expenseService.editExpense(expense, id);
+    public ResponseEntity<?> editExpense(@RequestBody Expense expense, @PathVariable("id") long id){
+        try {
+            Expense data = expenseService.editExpense(expense, id);
+            return new ResponseEntity<>(data ,HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 }
